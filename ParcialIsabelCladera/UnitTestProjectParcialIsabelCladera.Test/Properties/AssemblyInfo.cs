b19fe8700c3e05ceb7c2cee95c6fc08f8bc7875b
AngelicaIsabelCladeraMelgar <a2019110253@estudiantes.upsa.edu.bo>
2020-09-19 12:42:46 -0400

    El unitTest

---
+++ ParcialIsabelCladera/UnitTestProjectParcialIsabelCladera.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectParcialIsabelCladera.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectParcialIsabelCladera.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0076de26-0e2f-4c43-81cc-81274f0f94fe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ParcialIsabelCladera/UnitTestProjectParcialIsabelCladera.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParcialIsabelCladera.Controllers;
+
+namespace UnitTestProjectParcialIsabelCladera.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            //Arrange
+            CountriesController countriesController = new CountriesController();
+
+            //Act
+            var ListaCountry = countriesController.GetCountries();
+
+            //Assert
+            Assert.IsNotNull(ListaCountry);
+        }
+    }
+}
